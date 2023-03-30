8d8f97f80055d90c88766c6b18d88ba0279c8a58
Illalla <94539204+Illalla@users.noreply.github.com>
2023-03-30 17:25:26 +0200

    Update helper_fns_for_torch.py

--- helper_fns_for_torch.py
+++ helper_fns_for_torch.py
@@ -1,10 +1,56 @@
 import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from torch import nn
 from tqdm.auto import tqdm
 import itertools
 from PIL import Image
 import random
+import os
+
+def create_dataloaders_from_directory(
+    train_dir: str,
+    test_dir: str,
+    train_transform: transforms.Compose,
+    test_transform: transforms.Compose,
+    batch_size: int,
+    num_workers: int=os.cpu_count()
+):
+  """Creates train and test DataLoaders of image data.
+  
+  Args:
+    train_dir: Path to training directory.
+    test_dir: Path to testing directory.
+    train_transform: torchvision transforms to be used for training data.
+    test_transform: torchvision transforms to be used for testing data.
+    batch_size: Number of samples per batch.
+    num_workers: Number of workers per DataLoader
+
+  Returns:
+    A tuple of (train_dataloader, test_dataloader, class_names).
+  """
+
+  train_data = datasets.ImageFolder(train_dir, transform=train_transform)
+  test_data = datasets.ImageFolder(test_dir, transform=test_transform)
+  class_names = train_data.classes
+
+  train_dataloader = DataLoader(
+      train_data,
+      batch_size=batch_size,
+      shuffle=True,
+      num_workers=num_workers,
+      pin_memory=True
+  )
+  test_dataloader = DataLoader(
+      test_data,
+      batch_size=batch_size,
+      shuffle=False,
+      num_workers=num_workers,
+      pin_memory=True
+  )
+  
+  return train_dataloader, test_dataloader, class_names
 
 def train_cls_model(model,
                     train_dataloader,
